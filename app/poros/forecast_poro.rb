a7f9357973904a406fc88e4b1f7e493f3fd89f37
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2021-04-25 20:32:47 -0600

    fix forecast poro

--- app/poros/forecast_poro.rb
+++ app/poros/forecast_poro.rb
@@ -1,25 +1,30 @@
 class ForecastPoro
-  attr_reader :current_weather, :daily_weather, :hourly_weather
+  attr_reader :current_weather,
+              :daily_weather,
+              :hourly_weather
 
   def initialize(data)
-    @current_weather = current(data[:current])
-    @daily_weather = daily(data[:daily][0..4])
-    @hourly_weather = hourly(data[:hourly][0..7])
+   @timezone_offset = data[:timezone_offset]
+   @current_weather = current(data[:current])
+   @daily_weather = daily(data[:daily][0..4])
+   @hourly_weather = hourly(data[:hourly][0..7])
   end
 
   def current(data)
-    WeatherPoro.new(data)
+    WeatherPoro.new(data, @timezone_offset, :datetime)
   end
 
   def daily(data)
-    data.map do |day|
-      WeatherPoro.new(day)
-    end
+   data.map do |day|
+     fields = day.slice(:dt, :sunrise, :sunset, :weather).merge(day[:temp].slice(:min, :max))
+     WeatherPoro.new(fields, @timezone_offset, :date)
+   end
   end
 
   def hourly(data)
     data.map do |hour|
-      WeatherPoro.new(hour)
+      fields = hour.slice(:dt, :temp, :weather)
+       WeatherPoro.new(fields, @timezone_offset, :time)
     end
   end
 end
