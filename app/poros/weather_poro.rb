428901ae767e77be6a67e43b1d6b04a2ba0f0313
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2021-04-25 20:24:25 -0600

    fix weather poro

--- app/poros/weather_poro.rb
+++ app/poros/weather_poro.rb
@@ -1,8 +1,12 @@
 class WeatherPoro
-  attr_reader :dt,
+  attr_reader :datetime,
+              :date,
+              :time,
               :sunrise,
               :sunset,
               :temperature,
+              :min_temp,
+              :max_temp,
               :feels_like,
               :humidity,
               :uvi,
@@ -11,15 +15,31 @@ class WeatherPoro
               :icon
 
   def initialize(data)
-    @dt = data[:dt]
-    @sunrise = data[:sunrise]
-    @sunset = data[:sunset]
-    @temperature = data[:temp]
-    @feels_like = data[:feels_like]
-    @humidity = data[:humidity]
-    @uvi = data[:uvi]
-    @visibility = data[:visibility]
-    @conditions = data[:weather][0][:description]
-    @icon = data[:weather][0][:icon]
+    set_datetime(data[:dt], timezone_offset, datetime)
+     @sunrise = local_time(data[:sunrise], timezone_offset) if data[:sunrise]
+     @sunset = local_time(data[:sunset], timezone_offset) if data[:sunset]
+     @temperature = data[:temp] if data[:temp]
+     @min_temp = data[:min] if data[:min]
+     @max_temp = data[:max] if data[:min]
+     @feels_like = data[:feels_like] if data[:feels_like]
+     @humidity = data[:humidity] if data[:feels_like]
+     @uvi = data[:uvi] if data[:uvi]
+     @visibility = data[:visibility] if data[:visibility]
+     @conditions = data[:weather][0][:description]
+     @icon = data[:weather][0][:icon]
+  end
+
+  def set_datetime(dt, offset, datetime_opt)
+    if datetime_opt == :datetime
+      @datetime = local_time(dt, offset)
+    elsif datetime_opt == :date
+      @date = local_time(dt, offset).split[0]
+    elsif datetime_opt == :time
+      @time = local_time(dt, offset).split[1]
+    end
+  end
+
+  def local_time(time, offset)
+    Time.at(time).getlocal(offset).to_s
   end
 end
